2026b1d98e2d9942afd24c194a324bf971550f5a
akanuma <erio@akanuma>
2020-08-15 23:51:57 -0700

    add: parameter estimation

---
+++ programs/mpc/horibe/bezier.py
@@ -0,0 +1,117 @@
+from scipy import integrate
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Coo():
+    def __init__(self,_x,_y):
+        self.x = _x
+        self.y = _y
+
+class Bezier():
+    def __init__(self,_p0,_p1,_p2,_p3):
+        self.p = [0]*4
+        self.p[0] = _p0
+        self.p[1] = _p1
+        self.p[2] = _p2
+        self.p[3] = _p3
+
+        self.fig = plt.figure(figsize=(10,5))
+        self.ax = self.fig.add_subplot(121)#111 1x1 
+        self.ax.set_xlim(-1.0 , 11.0)
+        self.ax.set_ylim(-1.0 , 11.0)
+
+        self.dax = self.fig.add_subplot(122)#111 1x1 
+
+        self.t = np.arange(0,1,0.01)
+
+        self.x = (self.t**3)*self.p[3].x + 3*(self.t**2)*(1-self.t)*self.p[2].x + 3*self.t*((1-self.t)**2)*self.p[1].x + ((1-self.t)**3)*self.p[0].x
+        self.y = (self.t**3)*self.p[3].y + 3*(self.t**2)*(1-self.t)*self.p[2].y + 3*self.t*((1-self.t)**2)*self.p[1].y + ((1-self.t)**3)*self.p[0].y
+
+        self.dx = 3*((self.t**2)*(self.p[3].x-self.p[2].x)+2*self.t*(1-self.t)*(self.p[2].x-self.p[1].x)+((1-self.t)**2)*(self.p[1].x-self.p[0].x))
+        self.dy = 3*((self.t**2)*(self.p[3].y-self.p[2].y)+2*self.t*(1-self.t)*(self.p[2].y-self.p[1].y)+((1-self.t)**2)*(self.p[1].y-self.p[0].y))
+
+        self.ddx = 6*(self.t*(self.p[3].x-2*self.p[2].x+self.p[1].x)+(1-self.t)*(self.p[2].x-2*self.p[1].x+self.p[0].x))
+        self.ddy = 6*(self.t*(self.p[3].y-2*self.p[2].y+self.p[1].y)+(1-self.t)*(self.p[2].y-2*self.p[1].y+self.p[0].y))
+
+        self.artists = [0]*4
+        self.artists[0], = self.ax.plot([self.p[0].x],[self.p[0].y],marker='.',markersize=10,label='p[0]')
+        self.artists[1], = self.ax.plot([self.p[1].x],[self.p[1].y],marker='.',markersize=10,label='p[1]')
+        self.artists[2], = self.ax.plot([self.p[2].x],[self.p[2].y],marker='.',markersize=10,label='p[2]')
+        self.artists[3], = self.ax.plot([self.p[3].x],[self.p[3].y],marker='.',markersize=10,label='p[3]')
+        self.ax_artist, = self.ax.plot(self.x,self.y)
+
+        dp = [((x_a-x_b)**2 + (y_a-y_b)**2)*100 for x_a,x_b,y_a,y_b in zip(self.x[:-1],self.x[1:],self.y[:-1],self.y[1:])]
+        adp = [(i**2 + j**2)**0.5 for i,j in zip(self.dx,self.dy)]
+        addp = [(i**2 + j**2)**0.5 for i,j in zip(self.ddx,self.ddy)]
+
+        R = (self.dx*self.ddy - self.ddx*self.dy) / ((self.dx**2 + self.dy**2)**1.5)
+
+        self.dax.set_xlim(-0.1 , 1.5)
+        self.dax.set_ylim(-1.0 , 40)
+        dp.append(0.0)
+
+        integrate_simps = integrate.simps(self.y, self.x)
+        self.dax_artist, = self.dax.plot(self.t,R)
+
+        dp_max_index = [i for i, x in enumerate(dp) if x == max(dp)]
+        adp_max_index = [i for i, x in enumerate(adp) if x == max(adp)]
+
+    def change(self):
+        self.x = (self.t**3)*self.p[3].x + 3*(self.t**2)*(1-self.t)*self.p[2].x + 3*self.t*((1-self.t)**2)*self.p[1].x + ((1-self.t)**3)*self.p[0].x
+        self.y = (self.t**3)*self.p[3].y + 3*(self.t**2)*(1-self.t)*self.p[2].y + 3*self.t*((1-self.t)**2)*self.p[1].y + ((1-self.t)**3)*self.p[0].y
+
+        self.dx = 3*((self.t**2)*(self.p[3].x-self.p[2].x)+2*self.t*(1-self.t)*(self.p[2].x-self.p[1].x)+((1-self.t)**2)*(self.p[1].x-self.p[0].x))
+        self.dy = 3*((self.t**2)*(self.p[3].y-self.p[2].y)+2*self.t*(1-self.t)*(self.p[2].y-self.p[1].y)+((1-self.t)**2)*(self.p[1].y-self.p[0].y))
+
+        self.ddx = 6*(self.t*(self.p[3].x-2*self.p[2].x+self.p[1].x)+(1-self.t)*(self.p[2].x-2*self.p[1].x+self.p[0].x))
+        self.ddy = 6*(self.t*(self.p[3].y-2*self.p[2].y+self.p[1].y)+(1-self.t)*(self.p[2].y-2*self.p[1].y+self.p[0].y))
+
+        dp = [((x_a-x_b)**2 + (y_a-y_b)**2)*100 for x_a,x_b,y_a,y_b in zip(self.x[:-1],self.x[1:],self.y[:-1],self.y[1:])]
+        adp = [(i**2 + j**2)**0.5 for i,j in zip(self.dx,self.dy)]
+        addp = [(i**2 + j**2)**0.5 for i,j in zip(self.ddx,self.ddy)]
+        R = ((self.dx**2 + self.dy**2)**1.5)/((self.dx*self.ddy - self.ddx*self.dy)**2)**0.5
+
+        self.ax_artist.set_data(self.x,self.y)
+        self.dax_artist.set_data(self.t,R)
+
+    def sets(self,_gco,_event):
+        for i,j in zip(self.artists,self.p):
+            if i == _gco:
+                i.set_data(_event.xdata,_event.ydata)
+                j.x = _event.xdata
+                j.y = _event.ydata
+                break
+        self.change()
+
+
+gco = None
+
+if __name__ == '__main__':
+    a = Coo(1,1)
+    b = Coo(10,1)
+    c = Coo(1,5)
+    d = Coo(10,5)
+
+    B = Bezier(a,b,c,d)
+
+    def motion(event):
+        global gco
+        if gco is None:
+            return
+        B.sets(gco,event)
+        plt.draw()
+
+    def onpick(event):
+        global gco
+        gco = event.artist
+
+
+    def release(event):
+        global gco
+        gco = None
+
+    plt.connect('motion_notify_event', motion)
+    plt.connect('pick_event', onpick)
+    plt.connect('button_release_event', release)
+
+    plt.show()
--- programs/mpc/horibe/mpc_test.py
+++ programs/mpc/horibe/mpc_test.py
@@ -6,12 +6,14 @@ import cvxopt
 from cvxopt import matrix
 import scipy.linalg
 
+import time
+
 """
 https://myenigma.hatenablog.com/entry/2017/02/07/084922
 https://qiita.com/taka_horibe/items/47f86e02e2db83b0c570
 """
 
-class modeling_tool:
+class ModelingTool:
     def __init__(self, A, B, N, Q, R, umax=None, umin=None, xmin=None, xmax=None):
         """
         solve MPC with modeling tool for test
@@ -25,21 +27,21 @@ class modeling_tool:
         costlist = 0.0
         constrlist = []
 
-        self.r = Parameter(nx, N)
-        self.x0 = Parameter(nx, 1)
+        self.r = cvxpy.Parameter((nx, N))
+        self.x0 = cvxpy.Parameter((nx, 1))
 
         for t in range(N):
             costlist += 0.5 * cvxpy.quad_form(self.x[:, t]-self.r[:,t], Q)
             costlist += 0.5 * cvxpy.quad_form(self.u[:, t], R)
 
-            constrlist += [x[:, t + 1] == A @ x[:, t] + B @ self.u[:, t]]
+            constrlist += [self.x[:, t + 1] == A @ self.x[:, t] + B @ self.u[:, t]]
 
             if xmin is not None:
                 constrlist += [self.x[:, t] >= xmin[:, 0]]
             if xmax is not None:
                 constrlist += [self.x[:, t] <= xmax[:, 0]]
 
-        costlist += 0.5 * cvxpy.quad_form(x[:, N], Q)  # terminal cost
+        costlist += 0.5 * cvxpy.quad_form(self.x[:, N], Q)  # terminal cost
 
         if xmin is not None:
             constrlist += [self.x[:, N] >= xmin[:, 0]]
@@ -55,9 +57,9 @@ class modeling_tool:
 
         self.prob = cvxpy.Problem(cvxpy.Minimize(costlist), constrlist)
 
-    def solve(x0, r):
+    def solve(self, x0, r):
         self.x0.value = x0
-        self.r = r
+        self.r.value = r
         self.prob.solve()
         return self.x.value, self.u.value
 
@@ -107,23 +109,42 @@ def use_modeling_tool(A, B, N, Q, R, x0, r, umax=None, umin=None, xmin=None, xma
     return x.value, u.value
 
 
+# class System:
+#     def __init__(self):
+#         self.vel = [0]
+#         self.acc = [0]
+#         self.pos = [0]
+#         self.dt = 0.01
+
+#     def tick(self, input):
+#         self.acc.append(input + 0.1 * np.random.randn())
+#         self.vel.append(self.vel[-1]+self.acc[-1]*self.dt + 0.01 * np.random.randn()*self.dt)
+#         self.pos.append(self.pos[-1]+self.vel[-1]*self.dt + 0.01 * np.random.randn()*self.dt)
+
+#     def getSize(self):
+#         return len(self.acc)
+    
+#     def getState(self):
+#         return (self.pos[-1]+ 0.02 * np.random.randn(), self.vel[-1]+ 0.1 * np.random.randn())
 class System:
     def __init__(self):
-        self.vel = [0]
+        self.state = np.array([[0], [0]], dtype="float") # (pos, vel)T
         self.acc = [0]
-        self.pos = [0]
         self.dt = 0.01
+        self.A = np.array([[1, self.dt], [0, 1]], dtype="float")
+        self.B = np.array([[0], [self.dt]], dtype="float")
 
-    def tick(self, input):
-        self.acc.append(input)
-        self.vel.append(self.vel[-1]+self.acc[-1]*self.dt)
-        self.pos.append(self.pos[-1]+self.vel[-1]*self.dt)
+    def tick(self, input_u):
+        current_s = self.state[:,-1].reshape(-1, 1)
+        next_s = np.matmul(self.A, current_s) + self.B * input_u
+        self.state = np.hstack((self.state, next_s))
+        self.acc = np.hstack((self.acc, input_u))
 
     def getSize(self):
-        return len(self.acc)
+        return self.state.shape[1]
     
     def getState(self):
-        return (self.pos[-1], self.vel[-1])
+        return self.state[:,-1]
 
 class PidControl:
     def __init__(self, target):
@@ -138,7 +159,7 @@ class PidControl:
         self.iterm += diff * self.dt
         dterm = (diff - self.last_diff) / self.dt
         self.last_diff = diff
-        output = -4.0 * diff - 2.0 * dterm
+        output = -10.0 * diff - 3.0 * dterm
         return min(max(output, -4.0), 4.0)
 
 class MpcControl:
@@ -148,9 +169,10 @@ class MpcControl:
         self.B = np.array([[0], [self.dt]])
         (nx, nu) = self.B.shape
         self.N = 10  # number of horizon
-        self.Q = np.matrix([[10000,0],[0,0.01]])
+        self.Q = np.matrix([[1000,0],[0,0.01]])
         self.R = np.eye(nu)
         self.target = target
+        self.modeling_tool =ModelingTool(self.A, self.B, self.N, self.Q, self.R, 4.0, -4.0)
 
     def control(self, index, state):
         x0 = np.array([[state[0]], [state[1]]])
@@ -163,13 +185,15 @@ class MpcControl:
             else:
                 r[0,j] = self.target[-1]
                 r[1,j] = 0
-        x, u = use_modeling_tool(self.A, self.B, self.N, self.Q, self.R, x0, r, 4.0, -4.0)
-        return np.array(u[0, 0])
+        
+        # x, u =use_modeling_tool(self.A, self.B, self.N, self.Q, self.R, x0, r, 4.0, -4.0)
+        x, u = self.modeling_tool.solve(x0, r)
+        return u[0,0]
 
 target = [0]
 
 time_size = 700
-delta = 0.00
+delta = 0.03
 for i in range(time_size):
     if i < 100:
         target.append(0.0 + delta * np.random.randn()) 
@@ -197,12 +221,15 @@ for i in range(time_size):
 system2 = System()
 mpc_control = MpcControl(target)
 output=0
+
+start = time.time()
 for i in range(time_size):
     if i%10 == 0:
         state = system2.getState()
         output = mpc_control.control(i, state)
     system2.tick(output)
-
+end = time.time()
+print ("elapsed_time:{0}".format(end - start) + "[sec]")
 
 # error = [0]
 # for i in range(time_size):
@@ -214,15 +241,15 @@ fig = plt.figure()
 
 ax_1 = fig.add_subplot(211)
 ax_1.plot(x, system1.acc, label="acc")
-ax_1.plot(x, system1.vel, label="vel")
-ax_1.plot(x, system1.pos, label="pos")
+ax_1.plot(x, system1.state[1,:], label="vel")
+ax_1.plot(x, system1.state[0,:], label="pos")
 ax_1.plot(x, target, label="target")
 plt.legend(loc='best')
 
 ax_2 = fig.add_subplot(212)
 ax_2.plot(x, system2.acc, label="acc")
-ax_2.plot(x, system2.vel, label="vel")
-ax_2.plot(x, system2.pos, label="pos")
+ax_2.plot(x, system2.state[1,:], label="vel")
+ax_2.plot(x, system2.state[0,:], label="pos")
 ax_2.plot(x, target, label="target")
 plt.legend(loc='best')
 
